f6ccd6c6a2e755e33c3150c29358c04581284309
RubensZaes <rubenszaes@gmail.com>
2023-09-21 16:35:17 -0300

    :sparkles: Classe que recupera o tokenJWT e verifica se é válido.

--- src/main/java/com/rubenszaes/agenda/config/security/TokenService.java
+++ src/main/java/com/rubenszaes/agenda/config/security/TokenService.java
@@ -1,8 +1,11 @@
 package com.rubenszaes.agenda.config.security;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.rubenszaes.agenda.domain.model.Usuario;
 import com.rubenszaes.agenda.exception.BusinessExcepition;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,7 +14,6 @@ import org.springframework.stereotype.Service;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
-import java.util.Date;
 
 @Service
 public class TokenService {
@@ -31,6 +33,19 @@ public class TokenService {
         } catch (JWTCreationException exception) {
 //            throw new RuntimeException("Erro ao gerar Token JWT", exception);
             throw new BusinessExcepition("Erro ao gerar Token JWT");
+        }
+    }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("API Agenda Service")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception){
+            throw new BusinessExcepition("TokenJWT Inválido ou Expeirado!");
         }
     }
 
