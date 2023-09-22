9132750335c0700caf58d732412a8465fd72cbe3
RubensZaes <rubenszaes@gmail.com>
2023-09-22 10:16:59 -0300

    :sparkles: Autenticando/Autorização de Usuário

--- src/main/java/com/rubenszaes/agenda/config/security/SecurityConfiguration.java
+++ src/main/java/com/rubenszaes/agenda/config/security/SecurityConfiguration.java
@@ -11,11 +11,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfiguration {
+
+    private final SecurityFilter securityFilter;
 
     private static final String[] AUTH_WHITELIST = {
             // -- Swagger UI v3
@@ -42,7 +45,8 @@ public class SecurityConfiguration {
                 .requestMatchers(AUTH_WHITELIST).permitAll()
                 .requestMatchers(AUTH_BLACKLIST).authenticated()
                 .anyRequest().authenticated()
-                .and().build();
+                .and().addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
--- src/main/java/com/rubenszaes/agenda/config/security/SecurityFilter.java
+++ src/main/java/com/rubenszaes/agenda/config/security/SecurityFilter.java
@@ -1,27 +1,42 @@
 package com.rubenszaes.agenda.config.security;
 
-import com.rubenszaes.agenda.exception.BusinessExcepition;
+import com.rubenszaes.agenda.domain.repository.UsuarioRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @Component
+@RequiredArgsConstructor
 public class SecurityFilter extends OncePerRequestFilter {
+
+    private final TokenService tokenService;
+
+    private final UsuarioRepository usuarioRepository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var tokenJWT = recuperarToken(request);
+        if (tokenJWT != null) {
+            var subject = tokenService.getSubject(tokenJWT);
+            var usuario = usuarioRepository.findByUsuario(subject);
+            var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
         filterChain.doFilter(request, response);
     }
 
     private String recuperarToken(HttpServletRequest request) {
         var authorizationHeader = request.getHeader("Authorization");
-        if (authorizationHeader == null)
-            throw new BusinessExcepition("Token JWT não enviado no cabeçalho Authorization");
-        return authorizationHeader.replace("Bearer ", "");
+        if (authorizationHeader != null)
+            return authorizationHeader.replace("Bearer ", "");
+        return null;
     }
 }
